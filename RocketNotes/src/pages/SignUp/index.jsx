1d4b574dab954ff0add7d65e37306fd5dc169121
João Paulo <joao.paulomonteiro@hotmail.com>
2023-04-10 22:51:14 -0300

    cadastro de usuário

--- RocketNotes/src/pages/SignUp/index.jsx
+++ RocketNotes/src/pages/SignUp/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import { api } from '../../services/api';
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
@@ -8,6 +12,26 @@ import { FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Background, Container, Form } from "./styles";
 
 export function SignUp() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    if(!name || !email || !password){
+      return alert("Preencha todos os campos!");
+    }
+
+    api.post("/users", { name, email, password })
+    .then(() => {alert('Usuário cadastrado com sucesso!')})
+    .catch(error => {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert('Não foi possível cadastrar')
+      }
+    })
+  }
   
   return (
     <Container>
@@ -20,24 +44,27 @@ export function SignUp() {
         <h2>Crie sua conta</h2>
 
         <Input 
-        placeholder="Nome"
-        type="text"
-        icon={FiUser}
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
-        placeholder="E-mail"
-        type="text"
-        icon={FiMail}
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
-        placeholder="Senha"
-        type="password"
-        icon={FiLock}
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
         
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={handleSignUp}/>
 
         <Link to='/'>
           Voltar para o login
