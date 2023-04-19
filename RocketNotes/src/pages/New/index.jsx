fe5ba70fb80d9c97731627c41a0d1e4d7e249a92
João Paulo <joao.paulomonteiro@hotmail.com>
2023-04-19 14:14:30 -0300

    Cadastrando notas

--- RocketNotes/src/pages/New/index.jsx
+++ RocketNotes/src/pages/New/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header'
@@ -9,15 +8,22 @@ import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
 import { Textarea } from '../../components/Textarea';
 
+import { api } from "../../services/api";
+
 import { Container, Form } from "./styles";
 
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink])
@@ -36,6 +42,18 @@ export function New() {
     setTags(prevState => prevState.filter(tag => tag !== deleted));
   }
 
+  async function handleNewNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    });
+
+    alert("Nota criada com sucesso!");
+    navigate("/")
+  }
+
 
   return (
     <Container>
@@ -48,8 +66,14 @@ export function New() {
             <Link to='/'>Voltar</Link>
           </header>
 
-          <Input placeholder="Título" />          
-          <Textarea placeholder="Observações" />
+          <Input 
+            placeholder="Título"
+            onChange={e => setTitle(e.target.value)}
+          />          
+          <Textarea
+            placeholder="Observações"
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
             {
@@ -92,7 +116,10 @@ export function New() {
             </div>
           </Section>
 
-          <Button title='Salvar' />
+          <Button 
+            title='Salvar'
+            onClick={handleNewNote}
+          />
           
         </Form>
       </main>
