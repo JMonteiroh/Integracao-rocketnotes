9237454ba63440921d5954a0fc472bf2f4564ac3
João Paulo <joao.paulomonteiro@hotmail.com>
2023-04-19 13:29:44 -0300

    Atualizar o avatar do usuário

--- RocketNotes/src/pages/Profile/index.jsx
+++ RocketNotes/src/pages/Profile/index.jsx
@@ -7,6 +7,10 @@ import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
+import { api } from "../../services/api";
+
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 import { useAuth } from "../../hooks/auth";
 
 import { Container, Form, Avatar } from "./styles";
@@ -19,6 +23,11 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState();
   const [passwordNew, setPasswordNew] = useState();
 
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
   async function handleUpdate() {
     const user = {
       name,
@@ -28,7 +37,16 @@ export function Profile() {
 
     }
 
-    await updateProfile({ user })
+    await updateProfile({ user, avatarFile })
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
+
   }
 
   return (
@@ -40,12 +58,13 @@ export function Profile() {
       </header>
         <Form>
           <Avatar>
-            <img src="https://github.com/jmonteiroh.png" alt="Foto do usuário" />
+            <img src={avatar} alt="Foto do usuário" />
             <label htmlFor="avatar">
               <FiCamera />
               <input 
                 id="avatar"
                 type="file"
+                onChange={handleChangeAvatar}
               />
             </label>
           </Avatar>
